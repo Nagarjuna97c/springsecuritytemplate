1f7b2d0294c1700764d32991b674c5b65f633dbc
Nagarjuna97c <nagarjuna97c@gmail.com>
2024-02-26 21:05:28 +0530

    Custom Authentication Provider added

---
+++ src/main/java/com/springsecurity/springsecuritytemplate/config/CustomAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.springsecurity.springsecuritytemplate.config;
+
+import com.springsecurity.springsecuritytemplate.models.User;
+import com.springsecurity.springsecuritytemplate.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        User user = userService.findUserByName(authentication.getName());
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if (user != null) {
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+                return new UsernamePasswordAuthenticationToken(userName, password, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        } else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
--- src/main/java/com/springsecurity/springsecuritytemplate/services/UserService.java
+++ src/main/java/com/springsecurity/springsecuritytemplate/services/UserService.java
@@ -3,33 +3,12 @@ package com.springsecurity.springsecuritytemplate.services;
 import com.springsecurity.springsecuritytemplate.models.User;
 import com.springsecurity.springsecuritytemplate.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 @AllArgsConstructor
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
   UserRepository userRepository;
-
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-      User user =(User) userRepository.findByName(username)
-        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-      List<GrantedAuthority> authorities;
-      String userName, password;
-      userName = user.getEmail();
-      password = user.getPassword();
-      authorities = new ArrayList<>();
-      authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
-
-      return new org.springframework.security.core.userdetails.User(userName,password,authorities);
-  }
 
     public User createUser(User request) {
         return userRepository.save(request);
@@ -37,5 +16,9 @@ public class UserService implements UserDetailsService {
 
     public User getUser(Long id) {
       return userRepository.findById(id).orElse(null);
+    }
+
+    public User findUserByName(String name) {
+        return (User) userRepository.findByName(name).orElse(null);
     }
 }
